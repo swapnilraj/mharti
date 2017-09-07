03e3ee4e8ec76c0a9eb0203f321ff8f8d4702404
Swapnil Raj <raj.swapnil99@gmail.com>
2017-09-08 00:43:51 +0530

    Add timer and date

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,36 @@
-console.log('test');
+
+/**
+ * HTMLDivELement
+ */
+const timer = document.getElementById('timer');
+const date = document.getElementById('current-date');
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const initializeTime = () => {
+  const time = new Date();
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+  const date = time.getDate();
+  const day = time.getDay();
+
+  updateDate(date, day);
+  updateClock(hour, minute, second);
+};
+
+/**
+* @param {number} hour
+* @param {number} minute
+* @param {number} second
+*/
+const updateClock = (hour, minute, second) => {
+  timer.innerText = `${hour}:${minute}:${second}`;
+};
+
+const updateDate = (currentDate, day) => {
+  date.innerText = `${days[day]}, ${currentDate}`;
+};
+
+setInterval(initializeTime, 1000);
+initializeTime();
+
